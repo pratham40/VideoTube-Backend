486afeee55aa6d2b7d9f0ca8fa7897e46e734de5
pratham40 <122166099+pratham40@users.noreply.github.com>
2024-09-26 18:31:13 +0530

    user and video model with jwt

---
+++ db/dbConnection.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../src/constant.js";
+
+const connectDB=async()=>{
+    try {
+        const connectionInstance=await mongoose.connect(`${process.env.MONGO_DB_URL}/${DB_NAME}`)
+        console.log(`MONGO DB CONNECTED ${connectionInstance.connection.host}`);
+    } catch (error) {
+        console.log(`connection error ${error.message}`);
+        process.exit(1);
+    }
+}
+
+export default connectDB
