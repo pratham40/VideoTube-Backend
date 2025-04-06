21b2d2ff77bff9736fb39550cab8e0da1b41dad8
pratham40 <122166099+pratham40@users.noreply.github.com>
2025-04-06 23:35:23 +0530

    add morgan and change file path in multer

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express"
 import cors from 'cors'
 import cookieParser from "cookie-parser"
+import morgan from "morgan"
 
 const app=express()
 
@@ -13,12 +14,11 @@ app.use(express.json({limit:'16kb'}))
 app.use(express.urlencoded({extended:true,limit:'16kb'}))
 app.use(express.static('public'))
 app.use(cookieParser())
-
+app.use(morgan('dev'))
 
 //routes import
 
 import userRouter from "./routes/user.routes.js"
-
 
 //routes declaration
 
