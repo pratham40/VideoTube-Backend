4d698daed4b55156d68e44aebd80ad440a2a6f1a
pratham40 <122166099+pratham40@users.noreply.github.com>
2025-04-08 20:57:58 +0530

    add getWatchHistory function to retrieve user's watch history with video owner details

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,7 @@ import { User } from "../models/user.model.js"
 import { destroyOnCloudinary, uploadOnCloudinary } from "../utils/cloudinary.js"
 import { ApiResponse } from "../utils/ApiResponse.js"
 import jwt from 'jsonwebtoken'
+import mongoose from "mongoose"
 
 const generateAccessAndRefreshToken = async (userId) => {
     try {
@@ -455,6 +456,55 @@ const getUserChannelProfile = asyncHandler(async (req,res,next) => {
         new ApiResponse(200,channel[0], "channel profile fetched successfully")
     )
 })
+
+const getWatchHistory = asyncHandler(async (req,res,next) => {
+
+    const user = await User.aggregate(
+        [
+            {
+                $match:{
+                    _id: new mongoose.Types.ObjectId(req.user._id)
+                }
+            },
+            {
+                $lookup:{
+                    from:"videos",
+                    localField:"watchHistory",
+                    foreignField:"_id",
+                    as:"watchHistory",
+                    pipeline:[
+                        {
+                            $lookup:{
+                                from:"users",
+                                localField:"owner",
+                                foreignField:"_id",
+                                as:"owner",
+                                pipeline:[
+                                    {
+                                        $project:{
+                                            fullName:1,
+                                            username:1,
+                                            avatar:1
+                                        }
+                                    }
+                                ]
+                            }
+                        }
+                    ]
+                }
+            }
+        ]
+    )
+
+    console.log('====================================');
+    console.log(user);
+    console.log('====================================');
+
+    return res.status(200).json(
+        new ApiResponse(200,user[0], "user watch history fetched successfully")
+    )
+})
+
 
 export {
     registerUser,
