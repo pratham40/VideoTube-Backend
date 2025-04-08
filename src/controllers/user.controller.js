f016ec7fe702bcbcc3e19d2a7a1b320b1587dd1f
pratham40 <122166099+pratham40@users.noreply.github.com>
2025-04-08 20:23:23 +0530

    add getUserChannelProfile function to retrieve user channel details with subscriber counts

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -380,6 +380,82 @@ const updateUserCoverImage = asyncHandler(async(req,res,next)=>{
     }
 })
 
+
+const getUserChannelProfile = asyncHandler(async (req,res,next) => {
+    const {username} = req.params
+
+    if (!username) {
+        throw new ApiError(400, "username is required")
+    }
+
+
+    // TODO: Revise Pipelines
+    const channel = await User.aggregate([
+        {
+            $match:{
+                username:username?.toLowerCase()
+            }
+        },
+        {
+            $lookup:{
+                from:"subscriptions",
+                localField:"_id",
+                foreignField:"channel",
+                as:"subscribers"
+            }
+        },
+        {
+            $lookup:{
+                from:"subscriptions",
+                localField:"_id",
+                foreignField:"subscriber",
+                as:"subscribedTo"
+            }
+        },
+        {
+            $addFields:{
+                subscribersCount:{
+                    $size:"$subscribers"
+                },
+                subscribedToCount:{
+                    $size:"$subscribedTo"
+                },
+                isSubscribed:{
+                    $cond:{
+                        if : {$in:[req.user?._id,"$subscribers"]},
+                        then:true,
+                        else:false
+                    }
+                }
+            }
+        },
+        {
+            $project:{
+                fullName:1,
+                username:1,
+                subscribersCount:1,
+                subscribedToCount:1,
+                isSubscribed:1,
+                avatar:1,
+                coverImage:1,
+                email:1,
+            }
+        }
+    ])
+    
+    console.log('====================================');
+    console.log(channel);
+    console.log('====================================');
+
+    if (!channel?.length) {
+        throw new ApiError(404, "channel not exist")
+    }
+
+    return res.status(200).json(
+        new ApiResponse(200,channel[0], "channel profile fetched successfully")
+    )
+})
+
 export {
     registerUser,
     loginUser,
