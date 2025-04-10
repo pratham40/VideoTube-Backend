55fbddc0501d05342e60e5d58e2d38227e5a6755
pratham40 <122166099+pratham40@users.noreply.github.com>
2025-04-10 13:15:22 +0530

    add comment model

---
+++ src/models/comment.model.js
@@ -0,0 +1,23 @@
+const { Schema, default: mongoose } = require("mongoose");
+const mongooseAggregatePaginate = require("mongoose-aggregate-paginate-v2");
+
+const commentSchema = new Schema({
+    content:{
+        type:String,
+        required:true
+    },
+    video:{
+        type:mongoose.Types.ObjectId,
+        ref:"Video"
+    },
+    Owner:{
+        type:mongoose.Types.ObjectId,
+        ref:"User"
+    }
+},{timestamps:true})
+
+
+commentSchema.plugin(mongooseAggregatePaginate)
+
+
+export const Comment = mongoose.model("Comment",commentSchema)
